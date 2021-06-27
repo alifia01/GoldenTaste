0f0735921a8b8b71f83eded8ac955210bf1c9ccb
alifia01 <brilliant11@webmail.umm.ac.id>
2021-06-27 22:33:49 +0700

    hapus hero

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -2,29 +2,6 @@
 @section('title', 'Blog')
 @section('content-blog')
 
-    <!-- Hero Section Begin -->
-    <section class="hero hero-normal">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-9">
-                    <div class="hero__search">
-                        <div class="hero__search__form">
-                            <form action="#">
-                                <div class="hero__search__categories">
-                                    All Categories
-                                    <span class="arrow_carrot-down"></span>
-                                </div>
-                                <input type="text" placeholder="What do yo u need?">
-                                <button type="submit" class="site-btn">SEARCH</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- Hero Section End -->
-
     <!-- Breadcrumb Section Begin -->
     <section class="breadcrumb-section set-bg" data-setbg="img/breadcrumb.jpg">
         <div class="container">
